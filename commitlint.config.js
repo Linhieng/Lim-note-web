bc11b3594b4028900bbfabb50fc2ab0440199e82
linhieng <790728740@qq.com>
2023-09-13 16:14:26 +0800

    add: 流程管理, husky+lint-staged+markdownlint

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [2, "always", ["chore", "add", "update", "move"]],
+        "type-case": [2, "always", "lower-case"],
+        "type-empty": [2, "never"],
+        "subject-empty": [2, "never"],
+        "subject-full-stop": [2, "never", "."],
+    },
+};
