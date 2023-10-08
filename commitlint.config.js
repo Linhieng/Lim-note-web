ddcec5a4eb67a5c85567d9cb0d47edf13fe363d9
linhieng <790728740@qq.com>
2023-10-08 17:58:43 +0800

    draft(vue): vue-cli 配置 svg-loader

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["chore", "add", "update", "move"]],
+        "type-enum": [2, "always", ["chore", "add", "update", "move", "draft"]],
         "type-case": [2, "always", "lower-case"],
         "type-empty": [2, "never"],
         "subject-empty": [2, "never"],
